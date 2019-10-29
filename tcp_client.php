986f0917fad3edcd568da42edc13ead83d54e2ae
党文杰 <dwj@golive.cc>
2019-10-29 14:01:32 +0800

    s

---
+++ tcp_client.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DWJ
+ * Date: 2019/10/29
+ * Time: 13:40
+ */
+//链接swool的服务
+$cilent = new swoole_client(SWOOLE_SCOK_TCP);
+
+$res = $client->connect('47.106.204.223',9501);
+if(!$res){
+    $client->close();
+    echo '连接失败';
+    exit;
+}
+
+//php cli常量
+fwrite(STDOUT,'请输入信息:');
+$msg = trim(fgets(STDIN));
+
+//发送信息给tcp service服务器
+$client->send($msg);
+
+//接受来自server的数据
+$result = $client->recv();
+echo $result;
