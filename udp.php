0a32a7b8be9adb0bcde36cd53ffe4697d1816f18
党文杰 <dwj@golive.cc>
2019-10-29 15:16:51 +0800

    s

--- udp.php
+++ udp.php
@@ -6,12 +6,13 @@
  * Time: 14:41
  */
 
-$serv = new swoole_server("0.0.0.0",9502,SWOOLE_PROCESS,SWOOLE_SOCK_UDP);
+$serv = new swoole_server("0.0.0.0", 9502, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 
-//监听数据接收时间
-$serv->on('Packet',function($serv,$data,$clientInfo){
-   $serv->sendto($clientInfo['address'], $clientInfo['port'],'Server'.$data) ;
-   var_dump($clientInfo);
+//监听数据接收事件
+$serv->on('Packet', function ($serv, $data, $clientInfo) {
+    $serv->sendto($clientInfo['address'], $clientInfo['port'], "Server ".$data);
+    var_dump($clientInfo);
 });
+
 //启动服务器
 $serv->start();
