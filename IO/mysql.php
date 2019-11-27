8a81086fec7d55ba5afa0cd67f0cf0b61e48228b
党文杰 <dwj@golive.cc>
2019-11-27 16:52:16 +0800

    's'

--- IO/mysql.php
+++ IO/mysql.php
@@ -13,30 +13,32 @@ class CoroutineMysql{
     protected  $mysqlSwoole;
     public function __construct()
     {
-        go(function (){
-            $this->mysqlSwoole =  new Co\MySQL();
-        });
 
     }
 
     public function query(){
-        $this->mysqlSwoole->connect(
-            [
+        go(function (){
+            $this->mysqlSwoole =  new Co\MySQL();
+            $server =   [
                 'host' => '47.106.204.223',
                 'port' => 3306,
                 'user' => 'root',
                 'password' => 'root',
                 'database' => 'swoole',
-            ]
-        );
+            ];
+            $this->mysqlSwoole->connect($server);
 
-        $res = $this->mysqlSwoole->query('SELECT * FROM user limit 10');
-        if($res === false){
-            echo '操作失败!';
-            exit;
-        }
+            $res = $this->mysqlSwoole->query('SELECT * FROM user limit 10');
 
-        var_dump($res);
+            if($res === false){
+                echo '操作失败!';
+                exit;
+            }
+
+            var_dump($res);
+
+        });
+
 
     }
 }
