b0af2d68944dad26068abd608bbaef4f893f8595
党文杰 <dwj@golive.cc>
2019-12-02 11:28:47 +0800

    's'

--- process/process.php
+++ process/process.php
@@ -7,7 +7,7 @@
  */
 $process = new swoole_process(function(swoole_process $pro){
 //echo 111;
-    $pro->exec('/usr/local/bin/php',[getcwd().'/http_server.php']);
+    $pro->exec('/usr/local/bin/php',[getcwd().'/../http_server.php']);
 },false);
 
 $pid = $process->start();
