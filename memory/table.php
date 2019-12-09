f0cc82a2bd983529e2b99c1b32a08d9c7bd766bf
党文杰 <dwj@golive.cc>
2019-12-09 15:04:25 +0800

    s

--- memory/table.php
+++ memory/table.php
@@ -10,8 +10,8 @@ $table = new Swoole\Table(1024);
 
 //内存表增加一行
 $table->column('id',TYPE_INT,8);
-$table->column('name',TYPE_STRING);
-$table->column('age',TYPE_INT);
+$table->column('name',TYPE_STRING,64);
+$table->column('age',TYPE_INT,8);
 $table->create();
 
 $table->set('1',[1,'dwj','11']);
